36d3ada65651cfeb3bc8b67b11b5d4eaadbb9c09
groggy <272930349@qq.com>
2019-11-01 21:07:12 +0800

    submit

--- php/ajax_php/addUser.php
+++ php/ajax_php/addUser.php
@@ -1,167 +1,50 @@
-<?php session_start(); 
-if(!isset($_SESSION['type'])||!isset($_SESSION['enter_id'])){
-    header('location:exit.php');
-    exit(); 
-}
-include("../ajax_php/connect.php");
-include("judgeid.php");
-if($_SESSION['type']==1){
-    @header("http/1.1 404 not found"); 
-    @header("status: 404 not found"); 
-    header('location:Error404.php');
-    exit(); 
-}
-if(!isset($_SESSION['college_id'])){
-    $sql="select * from college";
-    $res = mysqli_query($db,$sql);
-    $row=$res->fetch_array();
-    $_SESSION['college_id']=$row[0];
-}
-if(!isset($_SESSION['dept_id'])){
-    $sql="select * from dept";
-    $res = mysqli_query($db,$sql);
-    $row=$res->fetch_array();
-    $_SESSION['dept_id']=$row[0];
-}
+<?php 
+	session_start();
+	header("content-type:text/html;charset=utf-8");         //设置编码
+	$json=file_get_contents("php://input");
+	$obj=json_decode($json);
+	include_once("connect.php");
+
+	$name=$obj->name;
+	$name=$db->real_escape_string($name);
+	$password=$obj->password;
+	$password=$db->real_escape_string($password);
+
+$private_key = "-----BEGIN PRIVATE KEY-----
+MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAMlOIrJxW0RLYgrD
+sOficnt/LasoWwh9jl9lhFhHfZbZUXFbQ7idwLaDYo1IDfsXNFCRrtD1f8qbutCH
+QB+huJjkP9Vkd2dN7JQL2/EzodS/Aph6Qwt9dIhoHli5QEPVHys2tGUtvCiWs3UD
++s8Z2C2ms8O5EshpaKy3BQB04gfVAgMBAAECgYEAipkTZoyJa/IC5KpraJwOelzL
+0qYMV0Iq/h9lCWrfWzbwzf0qGTfz8TVwaxmLFq+ZQ0eqdxAwFg02iFA2pBCyiMhy
+s1ZFsRVERGk2Qo8dQb2ly3XsngHtB/j7oBJVcWvwD+JtSWkBWLYPZqpkM5uU3hD6
+fEAVAmVHRjq6n7LwccECQQDzYzcYwwMa9h6m2BrEYlDQpI3Cl/jtUGNSiOWVl+nq
+eBqDanzfwX6/yywNLjKVKns00zxolEG7edOeSPPyfY1pAkEA07ynr37tjJua17rA
+uE0dllcAHA4Ko8boMjbvOwBNp2ypxNAtdodyjIEdJJ/7zOZJX1JSILRu3EPFmjjL
+pvxdjQJAKfYqEp/UkjpqsHNDsiYNLtugATO4XBnm9dzaUD8/ugf48j1SyDURCDoc
+Hy2e1O7dDQ96M8GTz6HCZWDIhj81OQJAWjx6UkaLwnLGSM4kN+dVhq7JMyugyS+J
+4WycA88bSRD8QQ5fcbZD0TFtVCCCVU6HUoJo0dtTq7eOTS2LTT0cOQJBAIuD+5s/
+TMnzjBKjuNaXohF2F3j54dSeEnvnUjXfLfyl8qL+9AwZaPeTZ320eWeixuSau26Z
+gl5DBWDZPTcL3OE=
+-----END PRIVATE KEY-----";
+	$hex_encrypt_data = base64_decode($password); //十六进制数据
+	
+	openssl_private_decrypt($hex_encrypt_data, $password, $private_key, OPENSSL_PKCS1_PADDING);
+	$password=sha1($password);
+
+	$account=$obj->account;
+	$class=$obj->class;
+
+	$db = db_connection("localhost","root","","money");
+	$select = "select count(*) from student where student_id='".$account."'and is_post=1";
+	$result = mysqli_query($db,$select);
+	$attr=$result->fetch_row();
+	if($attr[0]!="0")
+		$flag=0;
+	else
+	{
+		$update = "insert into student (student_id,password,class,is_post,name) values ('".$account."','".$password."','".$class."',1,'".$name."')";
+		$result = mysqli_query($db,$update);
+	}
+	
+	echo json_encode(array("flag"=>$flag,"result"=>$update));
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>人员添加</title>
-
-    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.css">
-
-    <link rel="stylesheet" type="text/css" href="../../css/admin_add.css">
-
-    <script type="text/javascript">
-        function changeCollege(){
-            var college=$("#college").val();
-            $.ajax({
-                url:"../ajax_php/changeCollege.php",
-                type:"post",
-                data:JSON.stringify({college:college}),
-                contentType:false,
-                processData:false,
-                success:function(data){
-                    window.location="admin_add.php";
-                },
-                error:function(){
-                    window.alert("error");
-                }
-            });
-        }     
-        function changeDept(){
-            var dept=$("#dept").val();
-            $.ajax({
-                url:"../ajax_php/changeDept.php",
-                type:"post",
-                data:JSON.stringify({dept:dept}),
-                contentType:false,
-                processData:false,
-                success:function(data){
-                    window.location="admin_add.php";
-                },
-                error:function(){
-                    window.alert("error");
-                }
-            });
-        }    
-    </script>
-</head>
-<body>
-    <?php include("nav.php")?>
-    <div class="jumbotron">
-        <div class="container">
-            <h2>人员信息添加</h2>
-            <p>填写人员基本信息; 上传并保留头像; 添加相关人员信息。</p>
-        </div>
-    </div>
-    <div id="mainber" class="container">
-        <div id="lefter">
-            <div id="accountInput" class="input-group">
-                <span class="input-group-addon">学号<span id="accountSpan"></span></span>
-                <input id="account" type="text" class="form-control" placeholder="只包含数字和字母 长度6-16">
-            </div>
-            <div id="passwordInput" class="input-group">
-                <span class="input-group-addon">密码<span id="passwordSpan"></span></span>
-                <input id="password" type="password" class="form-control" placeholder="长度3-10">
-            </div>
-            <div id="nameInput" class="input-group">
-                <span class="input-group-addon">姓名<span id="nameSpan"></span></span>
-                <input id="name" type="text" class="form-control" placeholder="非空">
-            </div>
-            <div id="collegeInput" class="input-group">
-                <span class="input-group-addon">学院<span id="collegeSpan"></span></span>
-                <select class="form-control" id="college" onchange="changeCollege()">
-                    <?php
-                    $sql="select * from college where college_id=".$_SESSION['college_id'];
-                    $res = mysqli_query($db,$sql);
-                    $attr=$res->fetch_array();
-                    $name=$attr[1];
-                    $sql="select * from college";
-                    $res = mysqli_query($db,$sql);
-                    echo"<option value='".$_SESSION['college_id']."'>".$name."</option>";
-                    while ($row=$res->fetch_array()) {
-                        if($row['college_id']==$_SESSION['college_id'])
-                            continue;
-                        echo"<option value='".$row['college_id']."'>".$row['college_name']."</option>";
-                    }
-                    ?>
-                </select>
-            </div>
-
-            <div id="deptInput" class="input-group">
-                <span class="input-group-addon">系别<span id="deptSpan"></span></span>
-                <select class="form-control" id="dept" onchange="changeDept()">
-                    <?php
-                    $sql="select * from dept where dept_id=".$_SESSION['dept_id'];
-                    $res = mysqli_query($db,$sql);
-                    $attr=$res->fetch_array();
-                    $name=$attr[1];
-                    $sql="select * from dept where college_id=".$_SESSION['college_id'];
-                    $res = mysqli_query($db,$sql);
-                    echo"<option value='".$_SESSION['dept_id']."'>".$name."</option>";
-                    while ($row=$res->fetch_array()) {
-                        if($row['dept_id']==$_SESSION['dept_id'])
-                            continue;
-                        echo"<option value='".$row['dept_id']."'>".$row['dept_name']."</option>";
-                    }
-                    ?>
-                </select>
-            </div>
-
-            <div id="classInput" class="input-group">
-                <span class="input-group-addon">班级<span id="classSpan"></span></span>
-                <select class="form-control" id="Class">
-                    <?php 
-                    $sql="select * from class where dept_id=".$_SESSION['dept_id'];
-                    $res = mysqli_query($db,$sql);
-                    while ($row=$res->fetch_array()) {
-                        echo"<option value='".$row['class_id']."'>".$row['class_name']."</option>";
-                    }
-                    ?>
-                </select>
-            </div>
-        </div>
-
-        <div id="righter">
-            <img id="Img" src="../../image/photo.png" alt="image">
-            <div>
-                <button type="button" class="btn btn-success" onclick="addUser()">添加</button>
-            </div>
-        </div>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"></script>
-    <script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="../../jsencrypt-master/bin/jsencrypt.min.js"></script>
-    <script src="../../js/My-Encryption.js" type="text/javascript" charset="utf-8"></script>
-    <script src="../../js/admin_add.js"></script>
-</body>
-
-</html>
